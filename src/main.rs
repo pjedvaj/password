2b7a08b8f01e9a3581e63dd69bcc1527174053ad
Petar Jedvaj <petar.jedvaj@outlook.com>
2025-01-24 09:36:43 +0100

    Random password code

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,25 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+
+    use rand::Rng;
+
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789)(*&^%$#@!~";
+                            
+    let mut password_length: usize = 14;
+
+    let mut password_range = rand::thread_rng();
+
+    let password: String = (0..password_length)
+        .map(|_| {
+            let index = password_range.gen_range(0..CHARSET.len());
+            CHARSET[index] as char
+        })
+        .collect();
+
+    println!("{:?}", password);
+
     ui.run()
 }
