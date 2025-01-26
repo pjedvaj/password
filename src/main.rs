7bc0144acd428dcea339c96c56f567a3f31f7c46
Petar Jedvaj <petar.jedvaj@outlook.com>
2025-01-26 20:26:18 +0100

    Charset options added

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,42 @@ fn main() -> Result<(), slint::PlatformError> {
 
     let ui_handle: Weak<AppWindow> = ui.as_weak();
 
+    //Allowed characters
+    const UPPERCASE: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const LOWERCASE: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+
+    const NUMBER: &[u8] = b"0123456789";
+
+    const SPECIAL: &[u8] = b")(*&^%$#@!~";
+
+    //Default use only uppercase letters
+    let mut charset:Vec<u8> = [UPPERCASE].concat();
+
+    //If checked also use lowercase letters
+    ui.on_lowercase_password(move || {
+        let mut charset:Vec<u8> = [UPPERCASE, LOWERCASE].concat();
+        // println!("lowercase toggled");
+    });
+
+    //If checked also use numbers
+    ui.on_number_password(move || {
+        let mut charset:Vec<u8> = [UPPERCASE, LOWERCASE, NUMBER].concat();
+        // println!("numbers toggled");
+    });
+
+    //If checked also use special characters
+    ui.on_special_password(move || {
+        let mut charset:Vec<u8> = [UPPERCASE, LOWERCASE, NUMBER, SPECIAL].concat();
+        // println!("special characters toggled");
+    });
+
+    //Main
     ui.on_generate_password(move || {
         let ui: AppWindow = ui_handle.unwrap();
 
         //Random pasword generator
         use rand::Rng;
-
-        //All allowed characters
-        const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                                abcdefghijklmnopqrstuvwxyz\
-                                0123456789)(*&^%$#@!~";
 
         // Password length - 16 characters
         let password_length: usize = 16;
@@ -29,8 +55,8 @@ fn main() -> Result<(), slint::PlatformError> {
     
         let password_string: String = (0..password_length)
             .map(|_| {
-                let index = password_range.gen_range(0..CHARSET.len());
-                CHARSET[index] as char
+                let index = password_range.gen_range(0..charset.len());
+                charset[index] as char
             })
             .collect();
     
